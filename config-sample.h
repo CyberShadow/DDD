cac5f5e01710371a5fcc7177a9cc34053d3f5798
Deadc0de <davide.by.zero@gmail.com>
2010-01-09 04:22:30 -0800

    * Implemented a special buffer size for the "all" file (should be larger than other stream buffers) * Modified the config sample to enable USE_ALL and to include default stream buffer sizes

--- config-sample.h
+++ config-sample.h
@@ -24,6 +24,10 @@
 // How much bytes of RAM to use?
 #define RAM_SIZE (8LL*1024*1024*1024)
 
+// How many bytes to use for each file stream buffer?
+#define STANDARD_BUFFER_SIZE (1*1024*1024 / sizeof(Node))
+#define ALL_FILE_BUFFER_SIZE (64*1024*1024 / sizeof(Node))
+
 // How many nodes to group under a single hash in the cache? Higher values reduce speed but allow better distribution.
 #define NODES_PER_HASH 4
 
@@ -38,9 +42,9 @@
 #endif
 
 // Keep a file with all nodes to filter against, instead of filtering against each individual frame. 
-// The "all" file contains the sorted contents of all "closed" nodes. 
-// Uses less RAM/CPU but more I/O (the "all" file needs to be rewritten every frame/frame-group).
-//#define USE_ALL
+// The "all" file contains the sorted contents of all "closed" nodes, and needs to be rewritten every frame/frame-group.
+// Enable USE_ALL to use less RAM/CPU and to optimize for sequential disk I/O. Disable USE_ALL if random disk access is very fast.
+#define USE_ALL
 
 // When free disk space falls below this amount, try to free up space by filtering open node files. If not defined, free space is not checked.
 //#define FREE_SPACE_THRESHOLD (50LL*1024*1024*1024)
