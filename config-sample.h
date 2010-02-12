ee77966ef671e9c076a8105904fba145048ef0f9
Deadc0de <davide.by.zero@gmail.com>
2010-02-12 08:40:40 -0800

    updated config-sample.h with recommended values for both pairs of modes relating to the Expanding phase

--- config-sample.h
+++ config-sample.h
@@ -9,6 +9,7 @@
 
 // Use this in combination with DISK_WINFILES to achieve more efficient disk I/O when the data set has gotten very large (however, this is slower with small data sets)
 //#define USE_UNBUFFERED_DISK_IO
+#define DISK_IO_CHUNK_SIZE (16*1024*1024)
 
 // MULTITHREADING will enable threading and synchronization code.
 #define MULTITHREADING
@@ -31,12 +32,27 @@
 #define STANDARD_BUFFER_SIZE (  1*1024*1024 / sizeof(Node)) // allocated separately in heap - used for open node files and other files
 
 // The expected ratio of Merging output/input
-#define EXPECTED_MERGING_RATIO 0.999
+#define EXPECTED_MERGING_RATIO 0.99
+
 
 // The parameters for the multithreaded Expansion queue/scheduler
+
 //#define DEBUG_EXPANSION
-#define EXPANSION_NODES_PER_QUEUE_ELEMENT 0x400
+//#define ENABLE_EXPANSION_SPILLOVER // buggy, drops a small number of nodes during Expansion; leave this disabled for now unless working on fixing it
+
+#ifdef DEBUG_EXPANSION
+#define EXPANSION_NODES_PER_QUEUE_ELEMENT (RAM_SIZE / sizeof(OpenNode) / 256)
+#else
+#define EXPANSION_NODES_PER_QUEUE_ELEMENT 0x1000
+#endif
+
+#ifdef ENABLE_EXPANSION_SPILLOVER
+#define EXPANSION_BUFFER_FILL_RATIO 0.98
+#define SPILLOVER_CHUNK_SIZE (2LL*1024*1024*1024)
+#else
 #define EXPANSION_BUFFER_FILL_RATIO (1./WORKERS)
+#endif
+
 
 // DISK_* selects the back-end storage to be used for the node disk files.
 #define DISK_WINFILES
