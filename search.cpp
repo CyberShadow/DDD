CyberShadow/DDD

commit 7238f3f0af440554e22d320f765c26dcae8490c7

print number of Expanded nodes after Merging

search.cpp
@@ -2978,7 +2978,7 @@ int search()
 		searchPrintHeader();
 		searchPrintNodeCounts();
 
-		printf("; (Resuming)                                                 "); fflush(stdout);
+		printf("; (Resuming)                                                        "); fflush(stdout);
 
 		time2 = time0;
 
@@ -3113,9 +3113,11 @@ int search()
 
 		ftime(&time2);
 		{
-			time_t ms       = (time2.time - time1.time)*1000 + (time2.millitm - time1.millitm);
-			time_t ms_total = (time2.time - time0.time)*1000 + (time2.millitm - time0.millitm);
-			printf("%4d.%03d s (%4d.%03d s)", ms/1000, ms%1000, ms_total/1000, ms_total%1000);
+			InputStream<Node> getSize(formatFileName("expanded", currentFrameGroup));
+			uint64_t expandedNodes = getSize.size();
+
+			time_t ms = (time2.time - time1.time)*1000 + (time2.millitm - time1.millitm);
+			printf("%4d.%03d s, %12llu nodes", ms/1000, ms%1000, expandedNodes);
 		}
 
 		if (checkStop(true))

commit c8249686cfa2ed5cf76b850e4e74f942c26389b0

Fix printing of number of Expanded nodes after Merging
This number was calculated as if the nodes were stored in closed format in this file (without frame numbers), but they are in fact stored in open format (with frame numbers) in this file.

Bug introduced in 7238f3f0af440554e22d320f765c26dcae8490c7

search.cpp
@@ -3663,7 +3663,7 @@ int search()
 
 		ftime(&time3);
 		{
-			InputStream<Node> getSize(formatFileName("expanded", currentFrameGroup));
+			InputStream<OpenNode> getSize(formatFileName("expanded", currentFrameGroup));
 			uint64_t expandedNodes = getSize.size();
 
 			time_t ms = (time3.time - time2.time)*1000 + (time3.millitm - time2.millitm);