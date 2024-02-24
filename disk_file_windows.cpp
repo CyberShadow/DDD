CyberShadow/DDD

commit f3769343b7d5c472c19bf1b8531a5aecb5bf1536

Make node alignment to 32 bits optional
It turns out this was never giving a speed-up. And it shouldn't have, because if any alignment would have, it'd have had to be 64 bits.

This currently probably only works on MSVC due to the use of "#pragma pack".

search.cpp
@@ -649,16 +649,24 @@ struct PackedCompressedState
 
 typedef PackedCompressedState BareNode;
 
+#ifndef ALIGN_TO_32BITS
+# pragma pack( push, 1 )
+#endif
+
 struct Node
 {
 	PackedCompressedState state;
 #ifdef GROUP_FRAMES
-# if COMPRESSED_BYTES%4 == 1
+# ifdef ALIGN_TO_32BITS
+#  if COMPRESSED_BYTES%4 == 1
 	uint8_t _align1;
+#  endif
 # endif
 	uint8_t subframe;
-# if (COMPRESSED_BYTES+(COMPRESSED_BYTES%4==1?1:0)+1)%4 != 0
+# ifdef ALIGN_TO_32BITS
+#  if (COMPRESSED_BYTES+(COMPRESSED_BYTES%4==1?1:0)+1)%4 != 0
 	uint8_t _align2[4-(COMPRESSED_BYTES+(COMPRESSED_BYTES%4==1?1:0)+1)%4];
+#  endif
 # endif
 #endif
 
@@ -668,13 +676,19 @@ struct Node
 struct OpenNode
 {
 	PACKED_FRAME frame;
-#if (COMPRESSED_BYTES+PACKED_FRAME_BYTES)%4 != 0
+#ifdef ALIGN_TO_32BITS
+# if (COMPRESSED_BYTES+PACKED_FRAME_BYTES)%4 != 0
 	uint8_t padding[4-(COMPRESSED_BYTES+PACKED_FRAME_BYTES)%4];
+# endif
 #endif
 	PackedCompressedState state;
 
 	CompressedState& getState() const { return (CompressedState&)state; }
 };
+
+#ifndef ALIGN_TO_32BITS
+# pragma pack( pop )
+#endif
 
 INLINE bool operator==(const Node& a, const Node& b) { return a.getState() == b.getState(); }
 INLINE bool operator!=(const Node& a, const Node& b) { return a.getState() != b.getState(); }

config-sample.h
@@ -74,6 +74,8 @@
 #define EXPANSION_BUFFER_FILL_RATIO (1./WORKERS)
 #endif
 
+//#define ALIGN_TO_32BITS
+
 
 // DISK_* selects the back-end storage to be used for the node disk files.
 #define DISK_WINFILES

commit 73b29befd9dbdee20917c5b5b4a7e2f56cc26768

Fix disabling of 32-bit alignment
Bug introduced in f3769343b7d5c472c19bf1b8531a5aecb5bf1536

disk_file_windows.cpp
@@ -20,6 +20,17 @@ void windowsError(const char* where = NULL)
 		error((LPCSTR)lpMsgBuf);
 }
 
+uint64_t getFileSize(const char* filename)
+{
+	HANDLE archive = CreateFile(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN | FILE_FLAG_NO_BUFFERING, NULL);
+	if (archive == INVALID_HANDLE_VALUE)
+		return 0;
+	ULARGE_INTEGER li;
+	li.LowPart = GetFileSize(archive, &li.HighPart);
+	CloseHandle(archive);
+	return li.QuadPart;
+}
+
 template<class NODE>
 class Stream
 {

search.cpp
@@ -4594,7 +4594,9 @@ void printExecutionTime()
 // Test the CompressedState comparison operators.
 void testCompressedState()
 {
+#ifdef ALIGN_TO_32BITS
 	enforce(sizeof(Node)%4 == 0);
+#endif
 #ifdef GROUP_FRAMES
 	enforce(COMPRESSED_BITS <= (sizeof(Node)-1)*8);
 #else
@@ -4610,6 +4612,7 @@ void testCompressedState()
 #ifdef GROUP_FRAMES
 	int subframe;
 
+#ifdef ALIGN_TO_32BITS
 	switch (COMPRESSED_BYTES % 4)
 	{
 		case 0: subframe = (int)sizeof(Node)-4; break;
@@ -4617,6 +4620,9 @@ void testCompressedState()
 		case 2: subframe = (int)sizeof(Node)-2; break;
 		case 3: subframe = (int)sizeof(Node)-1; break;
 	}
+#else
+	subframe = (int)sizeof(Node)-1;
+#endif
 
 	p1[subframe] = 0xFF;
 	enforce(c1 == c2, "Different subframe causes inequality");