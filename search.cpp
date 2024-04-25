e5e7d1a1efe17f9f1988b7d1bfbfc61fb45f8beb
Vladimir Panteleev <git@cy.md>
2024-04-25 16:38:20 +0000

    Comment out the __in annotations
    They don't do anything anyway, and not all compilers ignore them.

--- search.cpp
+++ search.cpp
@@ -4564,7 +4564,7 @@ int writePartialSolution()
 
 #if defined(_WIN32)
 #pragma comment(lib,"user32")
-DWORD WINAPI idleWatcher(__in LPVOID lpParameter)
+DWORD WINAPI idleWatcher(/*__in*/ LPVOID lpParameter)
 {
     LASTINPUTINFO lii;
     lii.cbSize = sizeof(LASTINPUTINFO);
