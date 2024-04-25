1d1de896940d5df32c8179933cf2543961c061c9
Vladimir Panteleev <git@cy.md>
2024-04-25 19:54:05 +0000

    thread_boost: Set thread ID
    This is mandatory for things to work.

--- thread_boost.cpp
+++ thread_boost.cpp
@@ -5,7 +5,10 @@
 template<void (*WORKER_FUNCTION)()>
 void THREAD_CREATE(THREAD_ID threadID)
 {
-	new boost::thread(WORKER_FUNCTION);
+	new boost::thread([threadID] {
+		TLS_SET_THREAD_ID(threadID);
+		WORKER_FUNCTION();
+	});
 }
 
 #define THREAD_JOIN(thread) (thread)->join()
